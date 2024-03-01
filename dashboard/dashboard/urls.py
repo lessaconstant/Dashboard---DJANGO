2832b6f7649a06323d662afe665c9a233116dfc6
Lucas Constant <lessaconstant@gmail.com>
2024-03-01 19:44:18 -0300

    mainpage + dashboard page + gitignore

--- dashboard/dashboard/urls.py
+++ dashboard/dashboard/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from dashboard_app.views import mainpage
+from dashboard_app.views import dashboards
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mainpage/', mainpage)
+    path('', mainpage),
+    path('/dashboards', dashboards, name='dashboards')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- dashboard/dashboard_app/views.py
+++ dashboard/dashboard_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 def mainpage(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'mainpage.html')
+
+def dashboards(request):
+    return render(request, 'dashboards.html')
 # Create your views here.
